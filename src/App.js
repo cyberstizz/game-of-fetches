b51a333bd85ae1ea020736b5b4c9b8529ae31c67
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-06 01:07:45 -0400

    added the second question

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <div><h2>Where was Margaery Tyrell born?</h2></div>
-      
+      <div><h2></h2></div>
+      <div><h2>What region is House Targaryen in?</h2></div>
+    
   );
 }
 
