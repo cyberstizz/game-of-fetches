5437b384fdabee5a523dac23337ce7099a2c7ad7
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-06 15:30:01 -0400

    added all classnames for api call positions

--- src/App.js
+++ src/App.js
@@ -5,19 +5,19 @@ function App() {
   return (
     <div className="App">
       <div><h2>Where was Margaery Tyrell born?</h2></div>
-      <div><h2></h2></div>
-      <div><h2>What region is House Targaryen in?</h2></div>
-      <div><h2></h2></div>
+      <div className='query1'><h2></h2></div>
+      <div ><h2>What region is House Targaryen in?</h2></div>
+      <div className='query2'><h2></h2></div>
       <div><h2>What's the coat of arms of House Lannister?</h2></div>
-      <div><h2></h2></div>
+      <div className='query3'><h2></h2></div>
       <div><h2>What is the second seat of House Baratheon?</h2></div>
-      <div><h2></h2></div>
+      <div className='query4'><h2></h2></div>
       <div><h2>What is Robert Baratheon's second alias?</h2></div>
-      <div><h2></h2></div>
+      <div className='query5'><h2></h2></div>
       <div><h2>What's the name of the founder of House Stark? (You have to chain fetch requests!)</h2></div>
-      <div><h2></h2></div>
+      <div className='query6'><h2></h2></div>
       <div><h2>What are the titles of Catelyn Stark's three POV books?</h2></div>
-      <div><h2></h2></div>
+      <div className='query7'><h2></h2></div>
     </div>
   );
 }
