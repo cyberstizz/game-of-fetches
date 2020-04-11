2dfbfa406e0511a330906aecb05806fa34f8b5ae
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-11 16:38:10 -0400

    screwed up folders and had to delet all components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import './App.css';
+import axios from 'axios';
+import QueryOne from '../components/queryOne.js';
 
 function App() {
+
+let spotOne = document.getElementById('query1')
+
+    
   return (
     <div className="App">
       <div><h2>Where was Margaery Tyrell born?</h2></div>
-      <div className='query1'><h2></h2></div>
+  <div id='query1'> {<QueryOne />} <h2>
+  
+  </h2></div>
       <div ><h2>What region is House Targaryen in?</h2></div>
-      <div className='query2'><h2></h2></div>
+      <div id='query2'><h2></h2></div>
       <div><h2>What's the coat of arms of House Lannister?</h2></div>
       <div className='query3'><h2></h2></div>
       <div><h2>What is the second seat of House Baratheon?</h2></div>
