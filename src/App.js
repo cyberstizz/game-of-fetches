c9e162c10ba7e567f314ff38e326a2c477058ea2
Student_GA106 <student_ga106@Student-GA106.local>
2020-04-11 20:07:45 -0400

    subtle changes

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import QueryOne from './components/queryOne'
 export default class App extends React.Component{
   constructor(props){
     super(props)
+    this.state = 
   }
     render(){
       return(
